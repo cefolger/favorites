5668602b7f06966ca4ff7f456d68b74c9b1fa9f6
Chris Folger <cefolger@gmail.com>
2012-10-20 00:11:41 -0400

    a basic layout

--- favorites/src/com/chrisfolger/favorites/view/mainview.py
+++ favorites/src/com/chrisfolger/favorites/view/mainview.py
@@ -3,5 +3,13 @@ import wx
 class MainView(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200,100))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+
+        self.firstButton = wx.Button(self, -1, "foo")
+        self.secondButton = wx.Button(self, -1, 'foo two')
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer.Add(self.firstButton, 1, wx.EXPAND)
+        self.sizer.Add(self.secondButton, 1, wx.EXPAND)
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(True)
+        self.sizer.Fit(self)        
         self.Show(True)
