89e455b439d14fa2407f7be435dc185ca1753a13
Chris Folger <cefolger@gmail.com>
2012-10-20 01:38:31 -0400

    start converting to Qt

--- favorites/src/com/chrisfolger/favorites/app.py
+++ favorites/src/com/chrisfolger/favorites/app.py
@@ -1,8 +1,13 @@
-import wx
-from view.mainview import MainView
-from controller.mainviewcontroller import start
-
-app = wx.App(False)
-frame = MainView(None, 'Small editor')
-start(frame)
-app.MainLoop()
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+ 
+ 
+# Create a Qt application
+app = QApplication(sys.argv)
+# Create a Label and show it
+label = QLabel("<font color=red size=40>Hello World</font>")
+label.show()
+# Enter Qt application main loop
+app.exec_()
+sys.exit()
