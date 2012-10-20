9e4b02792b911ae95c2c9d555231d75e1b0e083e
Chris Folger <cefolger@gmail.com>
2012-10-20 17:37:16 -0400

    add commits list

--- favorites/src/com/chrisfolger/favorites/view/console.py
+++ favorites/src/com/chrisfolger/favorites/view/console.py
@@ -1,16 +1,17 @@
-from PySide.QtGui import QWidget
-from PySide.QtGui import QDialog
 from PySide.QtGui import QTextEdit
-from PySide.QtGui import QTreeWidget
-from PySide.QtGui import QHBoxLayout
-from PySide.QtGui import QLabel
+from PySide.QtGui import QListWidget
 from PySide.QtGui import QTabWidget
-from PySide.QtGui import QSizePolicy
 
 class Console():
     def __init__(self, targetLayoutContainer):
         self.textarea = QTextEdit()
-        targetLayoutContainer.addWidget(self.textarea)
+        self.commits = QListWidget()
+        
+        self.widget = QTabWidget()
+        self.widget.addTab(self.textarea, 'Log')
+        self.widget.addTab(self.commits, 'Commits')
+        
+        targetLayoutContainer.addWidget(self.widget)
         
     def color(self, module, function, color,  *args):
         print module, function, args
