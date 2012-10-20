809ad51afc4b11e05016ed300c56239b06fe8c6d
Chris Folger <cefolger@gmail.com>
2012-10-20 00:22:59 -0400

    try out treectrl

--- favorites/src/com/chrisfolger/favorites/view/mainview.py
+++ favorites/src/com/chrisfolger/favorites/view/mainview.py
@@ -2,14 +2,17 @@ import wx
 
 class MainView(wx.Frame):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        wx.Frame.__init__(self, parent, title=title, size=(800,600))
 
-        self.firstButton = wx.Button(self, -1, "foo")
+        
+        self.firstButton = wx.TreeCtrl(self, wx.ID_ANY)
+        id = self.firstButton.AddRoot('hello')
+        self.firstButton.AppendItem(id, 'foo')
         self.secondButton = wx.Button(self, -1, 'foo two')
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.sizer.Add(self.firstButton, 1, wx.EXPAND)
-        self.sizer.Add(self.secondButton, 1, wx.EXPAND)
+        self.sizer.Add(self.secondButton, 3, wx.EXPAND)
         self.SetSizer(self.sizer)
         self.SetAutoLayout(True)
-        self.sizer.Fit(self)        
+        #self.sizer.Fit(self)        
         self.Show(True)
