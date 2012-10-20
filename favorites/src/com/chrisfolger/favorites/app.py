56399e5fd7b535fe4523aee7dbb8b68fda3381cc
Chris Folger <cefolger@gmail.com>
2012-10-20 01:41:29 -0400

    start conversion of MainForm to qt

--- favorites/src/com/chrisfolger/favorites/app.py
+++ favorites/src/com/chrisfolger/favorites/app.py
@@ -1,13 +1,14 @@
 import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
+from view.mainview import MainView
  
  
 # Create a Qt application
 app = QApplication(sys.argv)
 # Create a Label and show it
-label = QLabel("<font color=red size=40>Hello World</font>")
-label.show()
+form = MainView()
+form.show()
 # Enter Qt application main loop
 app.exec_()
 sys.exit()
--- favorites/src/com/chrisfolger/favorites/view/mainview.py
+++ favorites/src/com/chrisfolger/favorites/view/mainview.py
@@ -1,22 +1,11 @@
-import wx
 from pages import HtmlFavoritePage
+from PySide.QtGui import QDialog
 
-class MainView(wx.Frame):
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(800,600))
-        self.treeCtrl = wx.TreeCtrl(self, wx.ID_ANY)
-        self.tabCtrl = wx.Notebook(self)
-        
-        page1 = HtmlFavoritePage(self.tabCtrl)
-        self.tabCtrl.AddPage(page1, 'test page')
-        
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.sizer.Add(self.treeCtrl, 1, wx.EXPAND)
-        self.sizer.Add(self.tabCtrl, 3, wx.EXPAND)
-        self.SetSizer(self.sizer)
-        self.SetAutoLayout(True)
-        self.Show(True)
-    
+class MainView(QDialog):
+    def __init__(self, parent=None):
+        super(MainView, self).__init__(parent)
+        self.setWindowTitle("My Form")
+
     def set_favorites(self, favoritesRoot):
         self.rootId = self.treeCtrl.AddRoot(favoritesRoot.label)
         self.add_children(favoritesRoot, self.rootId)
