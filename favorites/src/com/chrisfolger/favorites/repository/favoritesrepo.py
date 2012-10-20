5f37d15339cc3e4f9287ff078f528d21ac463450
Chris Folger <cefolger@gmail.com>
2012-10-20 14:08:26 -0400

    recursively generates favorites

--- favorites/src/com/chrisfolger/favorites/repository/favoritesrepo.py
+++ favorites/src/com/chrisfolger/favorites/repository/favoritesrepo.py
@@ -6,17 +6,34 @@ class FavoritesRepository:
     def __init__(self, logger):
         self.logger = logger     
     
+    def get_favorite_from_directory(self, directory):
+        titleFile = open(directory + '/title', 'r')
+        title = titleFile.readline()
+        titleFile.close()
+        
+        favorite = Favorite(title)
+        return favorite
+    
     def get_favorites_root(self, directory):
         favoritesDirectory = directory + '/favorites'
         self.logger.info(__name__, ' get_favorites_root',' grabbing all favorites from ', directory + '/favorites')
         rootFavoriteEntry = [name for name in os.listdir(directory + '/favorites') if os.path.isdir(os.path.join(directory + '/favorites', name))][0]
         
-        # get the title
-        titleFile = open(favoritesDirectory + '/' + rootFavoriteEntry + '/title', 'r')
-        title = titleFile.readline()
-        titleFile.close()
-        favorite = Favorite(title)
+        favorite = self.get_favorite_from_directory(favoritesDirectory + '/' + rootFavoriteEntry)
        
+        for child in [name for name in os.listdir(favoritesDirectory + '/' + rootFavoriteEntry) if os.path.isdir(os.path.join(favoritesDirectory + '/' + rootFavoriteEntry, name))]:
+            favorite.children.append(self.get_favorites_children(os.path.join(favoritesDirectory + '/' + rootFavoriteEntry, child)))
+       
+        return favorite
+    
+    def get_favorites_children(self, currentNodeDirectory):
+        self.logger.info(__name__, 'get_favorites_children', currentNodeDirectory)
+        
+        favorite = self.get_favorite_from_directory(currentNodeDirectory)
+        
+        for child in [name for name in os.listdir(currentNodeDirectory) if os.path.isdir(os.path.join(currentNodeDirectory, name))]:
+            favorite.children.append(self.get_favorites_children(os.path.join(currentNodeDirectory, child)))
+        
         return favorite
     
     def new_repo(self, directory):
