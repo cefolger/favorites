63ef52d06f2c7232d0d2c8a2fa880fea6d21c703
Chris Folger <cefolger@gmail.com>
2012-10-20 21:21:36 -0400

    work on html page editor

--- favorites/src/com/chrisfolger/favorites/view/pages.py
+++ favorites/src/com/chrisfolger/favorites/view/pages.py
@@ -1,5 +1,9 @@
 from PySide.QtGui import QWidget
 from PySide.QtGui import QLabel
+from PySide.QtGui import QLineEdit
+from PySide.QtGui import QVBoxLayout
+from PySide.QtGui import QHBoxLayout
+from PySide.QtGui import QPushButton
 
 def get_page_widget(page, layout):
     if page.url:
@@ -8,8 +12,20 @@ def get_page_widget(page, layout):
 class HtmlFavoritePage():
     def __init__(self, page, layout):
         self.page = page
-        label = QLabel('<a href="' + page.url + '">Click here to open the URL</a>')
+        
+        container = QVBoxLayout()
+        # actions section
+        actionsContainer = QHBoxLayout()
+        button = QPushButton('Save')
+        actionsContainer.addWidget(QLabel('Link: '))
+        actionsContainer.addWidget(QLineEdit(page.url))
+        actionsContainer.addWidget(button)
+        label = QLabel('<a href="' + page.url + '">Open URL</a>')
         label.setOpenExternalLinks(True)
-        layout.addWidget(label)
-        label.show()
+        actionsContainer.addWidget(label)
+        container.addLayout(actionsContainer)
+        # content section 
+        # properties section 
+        
+        layout.addLayout(container)
   
--- favorites/src/com/chrisfolger/favorites/view/tabview.py
+++ favorites/src/com/chrisfolger/favorites/view/tabview.py
@@ -23,7 +23,7 @@ class TabView():
         widget.setLayout(layout)
         
         pageWidget = pages.get_page_widget(page, layout)
-        self.tabs.addTab(widget, page.label)
-        widget.show()
+        self.tabs.insertTab(0, widget, page.label)
+        self.tabs.setCurrentIndex(0)
         
         
